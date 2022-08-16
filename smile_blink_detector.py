a05b6246f7bd745a8f77cccf994dee74687ec8c8
Uma-kanagarajan <39462776+Uma-kanagarajan@users.noreply.github.com>
2022-08-16 14:15:45 +0800

    Add files via upload

---
+++ smile_blink_detector.py
@@ -0,0 +1,161 @@
+# import the necessary packages
+import argparse
+import cv2
+import dlib
+import imutils
+import numpy as np
+from imutils import face_utils
+from imutils.video import FileVideoStream, VideoStream
+import time
+from scipy.spatial import distance as dist
+
+# Initialize constant and variables
+SAR_THRESH = 0.44
+EYE_THRESH = 0.3
+EYE_CONSEC_FRAMES = 3
+counter = 0
+r_counter = 0
+l_counter = 0
+total = 0
+r_total = 0
+l_total = 0
+
+# function to calculate 'smile aspect ratio'
+def smile_aspect_ratio(mouth, jaw):
+    jaw_length = dist.euclidean(jaw[0], jaw[16])
+    mouth_length = dist.euclidean(mouth[0], mouth[6])
+    sar = mouth_length / jaw_length
+    return sar
+
+# function to calculate 'eye aspect ratio'
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
+# ap.add_argument("-v", "--video", type=str, default="", help="path to input video file")
+args = vars(ap.parse_args())
+
+# initialize dlib's face detector (HOG-based) and then create the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# Extract facial landmarks for mouth, jaw, left and right eye
+(m_start, m_end) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+(j_start, j_end) = face_utils.FACIAL_LANDMARKS_IDXS["jaw"]
+(l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# Initialize the camera stream
+print("[INFO] starting camera stream thread...")
+vs = VideoStream(src=0).start()
+time.sleep(1.0)
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame, resize it and convert to grayscale
+    frame = vs.read()
+    frame = imutils.resize(frame, width=800)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale frame
+    faces = detector(gray, 0)
+
+    # loop over the face detections
+    for face in faces:
+        # determine the facial landmarks for the face region, then
+        # convert the landmark (x, y)-coordinates to a NumPy array
+        shape = predictor(gray, face)
+        shape = face_utils.shape_to_np(shape)
+
+        # get the mouth and jaw values in the array and calculate the sar
+        mouth_shape = shape[m_start:m_end]
+        jaw_shape = shape[j_start:j_end]
+        sar = smile_aspect_ratio(mouth_shape, jaw_shape)
+
+        # drawing mouth contours
+        mouth_hull = cv2.convexHull(mouth_shape)
+        cv2.drawContours(frame, [mouth_hull], -1, (0, 255, 0), 1)
+
+        # get the left and right eye values and calculate ear
+        left_eye = shape[l_start:l_end]
+        right_eye = shape[r_start:r_end]
+        left_ear = eye_aspect_ratio(left_eye)
+        right_ear = eye_aspect_ratio(right_eye)
+        ear = (left_ear + right_ear) / 2.0
+
+        # drawing left and right eye contours
+        left_eye_hull = cv2.convexHull(left_eye)
+        right_eye_hull = cv2.convexHull(right_eye)
+        cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
+
+        # check sar and ear against respective threshold values
+        if sar > SAR_THRESH:
+            cv2.putText(
+                            frame,
+                            "Smiling",
+                            (400, 150),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0, 0, 255),
+                            2,
+                        )
+
+        if ear < EYE_THRESH:
+            counter += 1
+        else:
+            if counter >= EYE_CONSEC_FRAMES:
+                total += 1
+            counter = 0
+
+        if EYE_THRESH > left_ear > right_ear:
+            l_counter += 1
+        else:
+            if l_counter >= EYE_CONSEC_FRAMES:
+                l_total += 1
+            l_counter = 0
+
+        if EYE_THRESH > right_ear > left_ear:
+            r_counter += 1
+        else:
+            if r_counter >= EYE_CONSEC_FRAMES:
+                r_total += 1
+            r_counter = 0
+
+        cv2.putText(
+            frame,
+            "Blinks: {} Left Wink: {} Right Wink: {}".format(total, l_total, r_total),
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 0, 255),
+            2,
+        )
+
+        cv2.putText(
+            frame,
+            "EAR: {:.2f} L-EAR: {:.2f} R-EAR: {:.2f} ".format(ear, left_ear, right_ear),
+            (10, 60),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 0, 255),
+            2,
+        )
+
+    # visualize the image
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # to close the image frame
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
---
+++ yolo_demo.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+
+capture = cv2.VideoCapture(0)
+
+# weight and height
+wht = 320
+# threshold confidence
+CONFIG_THRESOLD = 0.5
+NMS_THRESHOLD = 0.3
+
+# open coco file
+class_path = 'coco.names'
+class_names = []
+with open(class_path, 'rt') as f:
+    class_names = f.read().rstrip('\n').split('\n')
+# print(class_names)
+
+# load the config file and weights
+model_config = 'yolov3.cfg'
+model_weights = 'yolov3.weights'
+
+net = cv2.dnn.readNetFromDarknet(model_config, model_weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
+
+# draw bounding box
+
+def find_objects(outputs, img):
+    height, weight, channel = img.shape
+    bbox = []
+    class_ids = []
+    confs = []
+
+    for output in outputs:
+        for det in output:
+            scores= det[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > CONFIG_THRESOLD:
+                w, h = int(det[2]*weight), int(det[3]*height)
+                x, y = int((det[0]*weight)-w/2), int((det[1]*height)-h/2)
+                bbox.append([x,y,w,h])
+                class_ids.append(class_id)
+                confs.append(float(confidence))
+
+    indices = cv2.dnn.NMSBoxes(bbox, confs, CONFIG_THRESOLD, NMS_THRESHOLD)
+    for i in indices:
+        box = bbox[i]
+        x,y,w,h = box[0], box[1], box[2], box[3]
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,255), 2)
+        cv2.putText(img, f'{class_names[class_ids[i]].upper()} {int(confs[i]*100)}%', (x,y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,255), 2)
+
+while True:
+    success, img = capture.read()
+
+    blob = cv2.dnn.blobFromImage(img, 1/255, (wht, wht), [0,0,0], 1, crop=False)
+    net.setInput(blob)
+
+    layer_names = net.getLayerNames()
+    # print(layerNames)
+    # print(net.getUnconnectedOutLayers())
+    output_names = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(output_names)
+    # print(len(outputs))
+    # print(outputs[0].shape)
+    # print(outputs[1].shape)
+    # print(outputs[2].shape)
+    # print(outputs[0][0])
+
+    find_objects(outputs, img)
+
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
